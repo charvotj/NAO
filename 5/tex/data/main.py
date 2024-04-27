bce7083e5756439812a60a7ece8865cf5bbf0a9e
charvotj <jakub.charvot@centrum.cz>
2024-04-27 22:24:39 +0200

    some more things done, gn

--- 5/tex/data/main.py
+++ 5/tex/data/main.py
@@ -70,10 +70,122 @@ def make_AC_plot():
     plt.savefig("5/tex/img/AC.pdf")
     plt.show()
 
-def make_second_plot(filename:str):
+def make_ICMR_plot():
+    data = pd.read_csv('5/tex/data/aplikace_ICMR.txt', delimiter='	')
+    
+    x_data = data['v_in']
+    y_data = data['V(u_out)']
+
+    fig, ax1 = plt.subplots()
+    
+    # Plot the original data
+    ax1.plot(x_data, y_data, label='Original Data', zorder=4)
+    ax1.set_xlabel(r'$U_{in} [V]$')
+    ax1.set_ylabel(r'$U_{out} [V]$')
+
+
+
+
+    # Compute first derivative (gradient)
+    dy_dx = np.gradient(y_data,x_data)
+    # dy_dx = np.gradient(dy_dx,x_data)
+
+    ax2 = ax1.twinx()
+    ax2.plot(x_data, dy_dx, label='First Derivative', linestyle='--', color='red', zorder=2)
+    ax2.set_ylabel('První derivace', color='red')
+    ax2.tick_params(axis='y', labelcolor='red')
+    ax2.set_ylim(bottom=0.9, top = 1.1)
+
+    th = 8e-4
+    # th = 0
+    center = 1
+    zero_indices = np.where((dy_dx >= center-th) & (dy_dx <= center+th) & (x_data > 0.2) & (x_data < 1.75))[0]
+
+    x = x_data[zero_indices[0]]
+    y = y_data[zero_indices[0]]
+    ax2.axvline(x=x, color='black', linestyle=':')
+    ax1.annotate(r'$U_{out}$='+f'{y:.2f} V ', 
+        xy=(x,y), 
+        xytext=(0.5,y), 
+        arrowprops=dict(facecolor='black', arrowstyle='->'), 
+        fontsize=10)
+    
+    x = x_data[zero_indices[-1]]
+    y = y_data[zero_indices[-1]]
+    ax2.axvline(x=x, color='black', linestyle=':')
+    ax1.annotate(r'$U_{out}$='+f'{y:.2f} V ', 
+        xy=(x,y), 
+        xytext=(0.6,y), 
+        arrowprops=dict(facecolor='black', arrowstyle='->'), 
+        fontsize=10)
+
+
+    # plt.xlim(0,1)
+    # plt.ylim(bottom=-0.2,top=2)
+    plt.grid(True)  # Add gridlines
+    plt.tight_layout()
+    plt.savefig("5/tex/img/ICMR.pdf")
+    plt.show()
+
+def make_OVS_plot():
+    data = pd.read_csv('5/tex/data/aplikace_OVS.txt', delimiter='	')
+    
+    x_data = data['v_in']
+    y_data = data['V(u_out)']
+
+    fig, ax1 = plt.subplots()
+    
+    # Plot the original data
+    ax1.plot(x_data, y_data, label='Original Data', zorder=4)
+    ax1.set_xlabel(r'$U_{in} [V]$')
+    ax1.set_ylabel(r'$U_{out} [V]$')
+
+
+
+
+    # Compute first derivative (gradient)
+    dy_dx = np.gradient(y_data,x_data)
+    dy_dx = np.gradient(dy_dx,x_data)
+
+    ax2 = ax1.twinx()
+    ax2.plot(x_data, dy_dx, label='Second Derivative', linestyle='--', color='red', zorder=2)
+    ax2.set_ylabel('Druhá derivace', color='red')
+    ax2.tick_params(axis='y', labelcolor='red')
+    ax2.set_ylim(bottom=-10, top = 10)
+
+    th = 0.6
+    zero_indices = np.where((dy_dx >= -th) & (dy_dx <= th) & (x_data > 0.8) & (x_data < 1))[0]
+
+    x = x_data[zero_indices[0]]
+    y = y_data[zero_indices[0]]
+    ax2.axvline(x=x, color='black', linestyle=':')
+    ax1.annotate(r'$U_{out}$='+f'{y:.2f} V ', 
+        xy=(x,y), 
+        xytext=(0.5,1.6), 
+        arrowprops=dict(facecolor='black', arrowstyle='->'), 
+        fontsize=10)
+    
+    x = x_data[zero_indices[-1]]
+    y = y_data[zero_indices[-1]]
+    ax2.axvline(x=x, color='black', linestyle=':')
+    ax1.annotate(r'$U_{out}$='+f'{y:.2f} V ', 
+        xy=(x,y), 
+        xytext=(1.1,y), 
+        arrowprops=dict(facecolor='black', arrowstyle='->'), 
+        fontsize=10)
+
+
+    # plt.xlim(0,1)
+    # plt.ylim(bottom=-0.2,top=2)
+    plt.grid(True)  # Add gridlines
+    plt.tight_layout()
+    plt.savefig("5/tex/img/OVS.pdf")
+    plt.show()
+
+def make_TRAN_plot():
     # Read the CSV file, skipping the first two rows
     # and considering the third row as the header
-    data = pd.read_csv(filename, delimiter='	')
+    data = pd.read_csv('5/tex/data/aplikace_TRAN.txt', delimiter='	')
     data['time'] *= 1e6
 
     # data = data.iloc[10:]
@@ -85,11 +197,23 @@ def make_second_plot(filename:str):
     plt.plot(x_data, y1_data, label='V(u_in)')
     plt.plot(x_data, y2_data, label='V(u_out)')
 
+    # SR rise
+    # y_values = [0.1*1.8, 0.9*1.8]
+    # x_values = [x_data[(x_data > 6 and y2_data >= y_values[0]).idxmax()], x_data[(x_data > 6 and y2_data >= y_values[1]).idxmax()]]
+    y_values = [0.1*1.8, 0.9*1.8]
+    x_values = [x_data[(y2_data >= y_values[0]).idxmax()], x_data[(y2_data >= y_values[1]).idxmax()]]
+    calc_value=abs(y_values[1]-y_values[0])/abs(x_values[1]-x_values[0])
+    plt.annotate(r'$SR_{rise}='+f'{calc_value:.2f} '+r'V/\mu s $', 
+                xy=(x_values[1],y_values[1]), 
+                xytext=(x_values[1]+0.2,y_values[1]-0.2), 
+                arrowprops=dict(facecolor='black', arrowstyle='->'), 
+                fontsize=10)
+
     # SR fall
     y_values = [0.1*1.8, 0.9*1.8]
     plt.axhline(y_values[0],color='black', linestyle=':')
     plt.axhline(y_values[1],color='black', linestyle=':')
-    x_values = [x_data[(y2_data <= y_values[0]).idxmax()], x_data[(y2_data <= y_values[1]).idxmax()]]
+    x_values = [x_data[((x_data > 6) & (y2_data <= y_values[0])).idxmax()], x_data[((x_data > 6) & (y2_data <= y_values[1])).idxmax()]]
     calc_value=abs(y_values[1]-y_values[0])/abs(x_values[1]-x_values[0])
     plt.annotate(r'$SR_{fall}='+f'{calc_value:.2f} '+r'V/\mu s $', 
                 xy=(x_values[0],y_values[0]), 
@@ -99,124 +223,15 @@ def make_second_plot(filename:str):
 
 
 
-    # SR rise
-    # y_values = [0.1*1.8, 0.9*1.8]
-    # x_values = [x_data[(x_data > 6 and y2_data >= y_values[0]).idxmax()], x_data[(x_data > 6 and y2_data >= y_values[1]).idxmax()]]
-    y_values = [0.1*1.8, 0.9*1.8]
-    x_values = [x_data[((x_data > 6) & (y2_data >= y_values[0])).idxmax()], x_data[((x_data > 6) & (y2_data >= y_values[1])).idxmax()]]
-    calc_value=abs(y_values[1]-y_values[0])/abs(x_values[1]-x_values[0])
-    plt.annotate(r'$SR_{rise}='+f'{calc_value:.2f} '+r'V/\mu s $', 
-                xy=(x_values[1],y_values[1]), 
-                xytext=(x_values[1]+0.2,y_values[1]-0.2), 
-                arrowprops=dict(facecolor='black', arrowstyle='->'), 
-                fontsize=10)
 
 
     plt.xlabel(r'$t [\mu s]$')
     plt.ylabel(r'$U [V]$')
     plt.legend()
-    # plt.xlim(0,1)
+    plt.xlim(0,10)
     # plt.ylim(bottom=0)
     plt.grid(True)  # Add gridlines
-    plt.savefig("4/tex/img/4-1-3.pdf")
-    plt.show()
-
-def make_third_plot(filename:str):
-    data = pd.read_csv(filename, delimiter='	')
-    
-    x_data = data['f']
-    y1_data = data['Au']
-    y2_data = data['phase']
-
-    plt.semilogx(x_data, y1_data, label='Id(Mp1)')
-
-    x = 1000
-    y = y1_data[(x_data >= x).idxmax()]
-    plt.annotate(r'$A_{U0}$='+f'{y:.2f} dB (f={(x/1000):.2f} kHz)', 
-                xy=(x,y), 
-                xytext=(x+0.2,y-5), 
-                arrowprops=dict(facecolor='black', arrowstyle='->'), 
-                fontsize=10)
-
-
-    y -= 3    # -3dB
-    x = x_data[(y1_data <= y).idxmax()-1]
-    plt.annotate(r'$A_{U}$='+f'{y:.2f} dB '+r'($f_{-3dB}$'+f'={(x/1e3):.2f} kHz)', 
-                xy=(x,y), 
-                xytext=(x-18.7e4,y-9), 
-                arrowprops=dict(facecolor='black', arrowstyle='->'), 
-                fontsize=10)
-    
-    y = 0  # GBW
-    x = x_data[(y1_data <= y).idxmax()]
-    plt.annotate(f'GBW={(x/1e6):.2f} MHz '+r'($A_{U}$='+f'{y:.2f} dB)', 
-                xy=(x,y), 
-                xytext=(x-16.4e6,y-8), 
-                arrowprops=dict(facecolor='black', arrowstyle='->'), 
-                fontsize=10)
-
-
-
-    plt.xlabel(r'f [Hz]')
-    plt.ylabel(r'$A_{U}$ [dB]')
-    # plt.legend()
-    
-    # plt.xlim(1,2)
-    # plt.ylim(bottom=0)
-    plt.grid(True)  # Add gridlines
-    plt.savefig("4/tex/img/4-2-2.pdf")
-    plt.show()
-
-
-def another_plot(filename:str):
-    # Read the CSV file, skipping the first two rows
-    # and considering the third row as the header
-    data = pd.read_csv(filename, delimiter='	')
-    data['time'] *= 1e6
-
-    # data = data.iloc[10:]
-    
-    x_data = data['time']
-    y1_data = data['V(u_in)']
-    y2_data = data['V(u_out)']
-    # Plot the data
-    plt.plot(x_data, y1_data, label='V(u_in)')
-    plt.plot(x_data, y2_data, label='V(u_out)')
-
-    # SR fall
-    y_values = [0.1*1.8, 0.9*1.8]
-    plt.axhline(y_values[0],color='black', linestyle=':')
-    plt.axhline(y_values[1],color='black', linestyle=':')
-    x_values = [x_data[(y2_data <= y_values[0]).idxmax()], x_data[(y2_data <= y_values[1]).idxmax()]]
-    calc_value=abs(y_values[1]-y_values[0])/abs(x_values[1]-x_values[0])
-    plt.annotate(r'$SR_{fall}='+f'{calc_value:.2f} '+r'V/\mu s $', 
-                xy=(x_values[0],y_values[0]), 
-                xytext=(x_values[0]-3,y_values[0]+0.2), 
-                arrowprops=dict(facecolor='black', arrowstyle='->'), 
-                fontsize=10)
-
-
-
-    # SR rise
-    # y_values = [0.1*1.8, 0.9*1.8]
-    # x_values = [x_data[(x_data > 6 and y2_data >= y_values[0]).idxmax()], x_data[(x_data > 6 and y2_data >= y_values[1]).idxmax()]]
-    y_values = [0.1*1.8, 0.9*1.8]
-    x_values = [x_data[((x_data > 6) & (y2_data >= y_values[0])).idxmax()], x_data[((x_data > 6) & (y2_data >= y_values[1])).idxmax()]]
-    calc_value=abs(y_values[1]-y_values[0])/abs(x_values[1]-x_values[0])
-    plt.annotate(r'$SR_{rise}='+f'{calc_value:.2f} '+r'V/\mu s $', 
-                xy=(x_values[1],y_values[1]), 
-                xytext=(x_values[1]+0.2,y_values[1]-0.2), 
-                arrowprops=dict(facecolor='black', arrowstyle='->'), 
-                fontsize=10)
-
-
-    plt.xlabel(r'$t [\mu s]$')
-    plt.ylabel(r'$U [V]$')
-    plt.legend()
-    # plt.xlim(0,1)
-    # plt.ylim(bottom=0)
-    plt.grid(True)  # Add gridlines
-    plt.savefig("4/tex/img/4-2-3.pdf")
+    plt.savefig("5/tex/img/TRAN.pdf")
     plt.show()
 
 
@@ -249,6 +264,9 @@ def make_latex_table(data:pd.DataFrame,filename):
 
 def main():
     make_AC_plot()
+    make_OVS_plot()
+    make_ICMR_plot()
+    make_TRAN_plot()
     # make_first_plot('4/tex/data/4-1-2ac.txt')
     # make_second_plot('4/tex/data/4-1-3tran.txt')
     # make_third_plot('4/tex/data/4-2-2ac.txt')
